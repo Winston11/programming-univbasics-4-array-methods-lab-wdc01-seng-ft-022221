eb4424a8133a469c08ce37d8f789539bfaf4ecff
Winston McGriff <winstonmcgriff27@gmail.com>
2021-02-10 18:20:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,20 +1,20 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+array.sort
 end
 
-def using_reverse(array)
-
+def using_reverse(array = [1, 2, 3])
+array.reverse
 end
 
-def using_first(array)
-
+def using_first(array = [2, 1, 3])
+array.first
 end
 
-def using_last(array)
+def using_last(array = [1, 2, 3])
 
 end
 
